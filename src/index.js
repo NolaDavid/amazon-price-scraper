496717eaf5c1a172e8e014d35cae0620326abb72
David Guillory <davidguillory@Davids-MacBook-Pro.local>
2021-12-26 14:29:50 -0700

    getting better

--- src/index.js
+++ src/index.js
@@ -1,52 +1,18 @@
-const axios = require('axios');
-const { JSDOM } = require('jsdom');
+import   axios   from 'axios';
+import cheerio from 'cheerio';
 
-const getProductUrl = (product_id) => `https://www.amazon.com/gp/aod/ajax/?asin=${product_id}&m=&smid=&sourcecustomerorglistid=&sourcecustomerorglistitemid=&sr=8-5&pc=dp`;
 
-async function getPrices(product_id) {
-  const productUrl = getProductUrl(product_id);
-  const { data: html } = await axios.get(productUrl, {
-    headers: {
-      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-      Host: 'www.amazon.com',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:88.0) Gecko/20100101 Firefox/88.0',
-      Pragma: 'no-cache',
-      TE: 'Trailers',
-      'Upgrade-Insecure-Requests': 1,
-    },
-  });
-  const dom = new JSDOM(html);
-  const $ = (selector) => dom.window.document.querySelector(selector);
-  const title = $('#aod-asin-title-text').textContent.trim();
-  const getOffer = (element) => {
-    const price = element.querySelector('.a-price .a-offscreen').textContent;
-    const offer_id = element.querySelector('input[name="offeringID.1"]').getAttribute('value');
-    const ships_from = element.querySelector('#aod-offer-shipsFrom .a-col-right .a-size-small').textContent.trim();
-    const sold_by = element.querySelector('#aod-offer-soldBy .a-col-right .a-size-small').textContent.trim();
-    const delivery_message = element.querySelector('#delivery-message').textContent.trim();
-    return {
-      price,
-      offer_id,
-      ships_from,
-      sold_by,
-      delivery_message: delivery_message.replace(/\s+/g, ' ').replace('Details', '').trim(),
-    };
-  };
-  const pinnedElement = $('#pinned-de-id');
 
-  const offerListElement = $('#aod-offer-list');
-  const offerElements = offerListElement.querySelectorAll('.aod-information-block');
-  const offers = [];
-  offerElements.forEach((offerElement) => {
-    offers.push(getOffer(offerElement));
-  });
-  const result = {
-    product_id,
-    title,
-    pinned: getOffer(pinnedElement),
-    offers,
-  };
-  console.log(result);
+const getProductUrl = (productId) => `hhttps://www.yeezysupply.com/api/products/${productId}`;
+async function getElements (productId)  {
+  const productUrl = getProductUrl(productId);
+  const  { data }  = await axios(productUrl,{
+    Headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+    }
+
+  })
+  console.log(data.product_description)
+ 
 }
-
-getPrices('B0743VZKYV');
+getElements('GW9524')
